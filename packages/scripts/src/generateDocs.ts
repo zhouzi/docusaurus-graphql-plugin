0933ed8cccde178786853b3e6798940ead2cc802
gabinaureche <hello@gabinaureche.com>
2021-04-18 17:23:24 +0200

    Untrack generated files

--- packages/scripts/src/generateDocs.ts
+++ packages/scripts/src/generateDocs.ts
@@ -2,42 +2,54 @@ import path from "path";
 import fs from "fs";
 import { Slugger } from "marked";
 
-const slugger = new Slugger();
-const sluggify = (name: string) => slugger.slug(name, { dryrun: true });
-const readme = fs.readFileSync(
-  path.join(__dirname, "..", "..", "..", "README.md"),
-  "utf-8"
-);
-const [_, ...pages] = readme.split(/\n/).reduce<Array<string[]>>(
-  (acc, line) => {
-    if (line.startsWith("## ")) {
-      acc.push([]);
-    }
+generateDocsFromReadme();
+copyReadmeToPackage();
 
-    acc[acc.length - 1].push(line);
-    return acc;
-  },
-  [[]]
-);
-pages.forEach((lines, index) => {
-  const [firstLine, ...content] = lines;
-  const title = firstLine.replace(/^##\s+/, "");
-
-  fs.writeFileSync(
-    path.join(__dirname, "..", "..", "docs", "docs", `${sluggify(title)}.md`),
-    [
-      "---",
-      `title: ${title}`,
-      ...(index === 0 ? ["slug: /"] : []),
-      "---",
-      "",
-      "<!--",
-      "This file was automatically generated from the README.md.",
-      "Do not edit it but make your changes to the README.md instead and run the command:",
-      "yarn --cwd ./packages/scripts generateDocs",
-      "-->",
-    ]
-      .concat(content)
-      .join("\n")
+function generateDocsFromReadme() {
+  const slugger = new Slugger();
+  const sluggify = (name: string) => slugger.slug(name, { dryrun: true });
+  const readme = fs.readFileSync(
+    path.join(__dirname, "..", "..", "..", "README.md"),
+    "utf-8"
   );
-});
+  const [_, ...pages] = readme.split(/\n/).reduce<Array<string[]>>(
+    (acc, line) => {
+      if (line.startsWith("## ")) {
+        acc.push([]);
+      }
+
+      acc[acc.length - 1].push(line);
+      return acc;
+    },
+    [[]]
+  );
+  pages.forEach((lines, index) => {
+    const [firstLine, ...content] = lines;
+    const title = firstLine.replace(/^##\s+/, "");
+
+    fs.writeFileSync(
+      path.join(__dirname, "..", "..", "docs", "docs", `${sluggify(title)}.md`),
+      [
+        "---",
+        `title: ${title}`,
+        ...(index === 0 ? ["slug: /"] : []),
+        "---",
+        "",
+        "<!--",
+        "This file was automatically generated from the README.md.",
+        "Do not edit it but make your changes to the README.md instead and run the command:",
+        "yarn --cwd ./packages/scripts generateDocs",
+        "-->",
+      ]
+        .concat(content)
+        .join("\n")
+    );
+  });
+}
+
+function copyReadmeToPackage() {
+  fs.copyFileSync(
+    path.join(__dirname, "..", "..", "..", "README.md"),
+    path.join(__dirname, "..", "..", "docusaurus-graphql-plugin", "README.md")
+  );
+}
