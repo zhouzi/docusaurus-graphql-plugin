a4d3099cb2be604508b5daa44c3276b6d7e151ad
Nick Vernij <hello@nickforall.nl>
2022-11-02 11:59:21 +0800

    Add code block escaping

--- packages/docusaurus-graphql-plugin/src/converters/parseMarkdown.ts
+++ packages/docusaurus-graphql-plugin/src/converters/parseMarkdown.ts
@@ -2,11 +2,15 @@ import marked from "marked";
 
 export function parseMarkdown(markdown: string): string {
   const walkTokens = (token: marked.Token) => {
+    // make the Markdown compatible with MDX by escaping curly braces
     if (token.type === "text") {
-      // make the Markdown compatible with MDX by escaping curly braces
       token.text = token.text
         .replace(/\{/g, "&lbrace;")
         .replace(/\}/g, "&rbrace;");
+    }
+
+    if (token.type === "codespan" || token.type === "code") {
+      token.text = token.text.replace(/\{/g, "&#123;").replace(/\}/g, "&#125;");
     }
   };
   return marked
